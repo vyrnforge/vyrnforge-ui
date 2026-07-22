4ac02e7fac77cc0a8a4d1e0c7cba43d13af8671c
SCC1202200800\wirya <darkzzangelzz@gmail.com>
2026-07-22 12:50:13 +0700

    Update vitest config

--- packages/ui-data-grid/vitest.config.ts
+++ packages/ui-data-grid/vitest.config.ts
@@ -2,27 +2,6 @@ import { defineConfig } from "vitest/config";
 import { fileURLToPath } from "node:url";
 
 export default defineConfig({
-  test: {
-    coverage: {
-      all: true,
-      exclude: [
-        "src/**/*.d.ts",
-        "src/**/*.types.ts",
-        "src/**/*.test.{ts,tsx}",
-        "src/**/__tests__/**",
-      ],
-      include: ["src/**/*.{ts,tsx}"],
-      provider: "v8",
-      reporter: ["text", "html", "json", "json-summary", "lcov"],
-      reportsDirectory: "../../coverage/ui-data-grid",
-      thresholds: {
-        branches: 32,
-        functions: 36,
-        lines: 38,
-        statements: 38,
-      },
-    },
-  },
   resolve: {
     alias: [
       {
@@ -39,4 +18,20 @@ export default defineConfig({
       },
     ],
   },
+  test: {
+    coverage: {
+      all: true,
+      exclude: ["src/**/*.d.ts", "src/**/*.types.ts", "src/**/*.test.{ts,tsx}", "src/**/__tests__/**"],
+      include: ["src/**/*.{ts,tsx}"],
+      provider: "v8",
+      reporter: ["text", "html", "json", "json-summary", "lcov"],
+      reportsDirectory: "../../coverage/ui-data-grid",
+      thresholds: {
+        branches: 32,
+        functions: 36,
+        lines: 38,
+        statements: 38
+      }
+    }
+  }
 });
