3079fd6a6673892a15b188575b24f7686acb37d0
draglide <darkzzangelzz@gmail.com>
2026-08-28 10:34:21 +0700

    feat(vue): create public package workspace for MFD-1301

---
+++ packages/ui-vue/src/index.ts
@@ -0,0 +1,15 @@
+export { VfButton } from "./generated/VfButton.generated";
+export type { VfButtonElement } from "./generated/VfButton.generated";
+
+export { VfDialog } from "./generated/VfDialog.generated";
+export type {
+  GeneratedDialogDismissDetail,
+  GeneratedDialogOpenChangeDetail,
+  VfDialogElement,
+} from "./generated/VfDialog.generated";
+
+export { VfTabs } from "./generated/VfTabs.generated";
+export type { VfTabsElement } from "./generated/VfTabs.generated";
+
+export { VfTextInput } from "./generated/VfTextInput.generated";
+export type { VfTextInputElement } from "./generated/VfTextInput.generated";
