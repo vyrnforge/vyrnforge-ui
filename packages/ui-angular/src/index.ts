c949af8f2615e47d817e9388909cf90f663edc4a
draglide <darkzzangelzz@gmail.com>
2026-08-28 23:29:42 +0700

    feat(angular): export generated catalog (MFD-1204)

--- packages/ui-angular/src/index.ts
+++ packages/ui-angular/src/index.ts
@@ -14,6 +14,8 @@ export type { VfTabsElement } from "./generated/vf-tabs.generated.js";
 export { VfTextInput } from "./generated/vf-text-input.generated.js";
 export type { VfTextInputElement } from "./generated/vf-text-input.generated.js";
 
+export * from "./generated/catalog.generated.js";
+
 export { provideVyrnForge } from "./setup.js";
 export type { VyrnForgeAngularOptions } from "./setup.js";
 
