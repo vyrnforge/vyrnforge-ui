ae983bb059f8bba9503b4c8c02d990e9bf9a3bc9
draglide <darkzzangelzz@gmail.com>
2026-08-28 17:44:19 +0700

    test(angular): consume one-step setup provider

--- tests/consumers/angular/src/main.ts
+++ tests/consumers/angular/src/main.ts
@@ -1,12 +1,11 @@
-import "@vyrnforge/ui-elements/register";
-
 import { provideZonelessChangeDetection } from "@angular/core";
 import { bootstrapApplication } from "@angular/platform-browser";
+import { provideVyrnForge } from "@vyrnforge/ui-angular";
 
 import { AppComponent } from "./app/app.component";
 
 bootstrapApplication(AppComponent, {
-  providers: [provideZonelessChangeDetection()],
+  providers: [provideZonelessChangeDetection(), provideVyrnForge()],
 }).catch((error: unknown) => {
   console.error(error);
 });
