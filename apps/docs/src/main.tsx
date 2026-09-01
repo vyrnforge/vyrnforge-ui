33cd57ca85e358a16523f174ce49cd0c20efc9ee
draglide <darkzzangelzz@gmail.com>
2026-09-01 21:38:30 +0700

    docs: load context selector styles

--- apps/docs/src/main.tsx
+++ apps/docs/src/main.tsx
@@ -4,9 +4,10 @@ import "@vyrnforge/ui-core/styles/index.css";
 import "@vyrnforge/ui-components/styles/index.css";
 import App from "./App";
 import "./styles/docs.css";
+import "./styles/docs-context.css";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
 );
