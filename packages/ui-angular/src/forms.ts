5a9525b0596ce30dc86835411cce2753791d1a6d
draglide <darkzzangelzz@gmail.com>
2026-09-06 21:40:21 +0700

    style(angular): apply exact forms formatting

--- packages/ui-angular/src/forms.ts
+++ packages/ui-angular/src/forms.ts
@@ -128,8 +128,15 @@ export class VyrnForgeFormControlDirective
 
   writeValue(value: unknown): void {
     const element = this.elementRef.nativeElement;
-    const converted = convertAngularFormValueToElement(element.localName, value);
-    this.renderer.setProperty(element, converted.property, converted.elementValue);
+    const converted = convertAngularFormValueToElement(
+      element.localName,
+      value,
+    );
+    this.renderer.setProperty(
+      element,
+      converted.property,
+      converted.elementValue,
+    );
     this.requestValidatorRefresh();
   }
 
