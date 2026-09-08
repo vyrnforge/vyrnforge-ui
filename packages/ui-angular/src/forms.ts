a5929f059cbee4ba79dffad110ee7669e87e3e2f
draglide <darkzzangelzz@gmail.com>
2026-09-08 10:16:08 +0700

    fix(angular): verify packed facade runtime

--- packages/ui-angular/src/forms.ts
+++ packages/ui-angular/src/forms.ts
@@ -3,6 +3,7 @@ import {
   ElementRef,
   Renderer2,
   forwardRef,
+  inject,
   type OnDestroy,
 } from "@angular/core";
 import {
@@ -95,39 +96,35 @@ function serializeValidity(
 export class VyrnForgeFormControlDirective
   implements ControlValueAccessor, OnDestroy, Validator
 {
-  private readonly listenerCleanup: ReadonlyArray<() => void>;
+  private readonly elementRef =
+    inject<ElementRef<VyrnForgeAngularFormElement>>(ElementRef);
+  private readonly renderer = inject(Renderer2);
+  private readonly listenerCleanup: ReadonlyArray<() => void> = [
+    this.renderer.listen(
+      this.elementRef.nativeElement,
+      "focusout",
+      (event: Event) => this.handleFocusOut(event as FocusEvent),
+    ),
+    this.renderer.listen(
+      this.elementRef.nativeElement,
+      "vf-checked-change",
+      (event: Event) => this.handleCheckedChange(event),
+    ),
+    this.renderer.listen(
+      this.elementRef.nativeElement,
+      "vf-invalid",
+      (event: Event) => this.handleInvalid(event),
+    ),
+    this.renderer.listen(
+      this.elementRef.nativeElement,
+      "vf-value-change",
+      (event: Event) => this.handleValueChange(event),
+    ),
+  ];
 
   private onChange: ChangeCallback = () => undefined;
   private onTouched: TouchedCallback = () => undefined;
   private onValidatorChange: ValidatorChangeCallback = () => undefined;
-
-  constructor(
-    private readonly elementRef: ElementRef<VyrnForgeAngularFormElement>,
-    private readonly renderer: Renderer2,
-  ) {
-    this.listenerCleanup = [
-      this.renderer.listen(
-        this.elementRef.nativeElement,
-        "focusout",
-        (event: Event) => this.handleFocusOut(event as FocusEvent),
-      ),
-      this.renderer.listen(
-        this.elementRef.nativeElement,
-        "vf-checked-change",
-        (event: Event) => this.handleCheckedChange(event),
-      ),
-      this.renderer.listen(
-        this.elementRef.nativeElement,
-        "vf-invalid",
-        (event: Event) => this.handleInvalid(event),
-      ),
-      this.renderer.listen(
-        this.elementRef.nativeElement,
-        "vf-value-change",
-        (event: Event) => this.handleValueChange(event),
-      ),
-    ];
-  }
 
   writeValue(value: unknown): void {
     const element = this.elementRef.nativeElement;
