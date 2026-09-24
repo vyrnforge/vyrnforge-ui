eba8dc1081cfdbf04c292a6e90fb0b5eec71f0e5
draglide <darkzzangelzz@gmail.com>
2026-09-24 10:22:41 +0700

    style: normalize PropertyTable React changes

--- packages/ui-components/src/components/__tests__/primitives.test.tsx
+++ packages/ui-components/src/components/__tests__/primitives.test.tsx
@@ -261,8 +261,18 @@ describe("@vyrnforge/ui-components primitives", () => {
       <PropertyTable aria-label="Deployment properties">
         <table>
           <caption>Deployment properties</caption>
-          <thead><tr><th scope="col">Property</th><th scope="col">Value</th></tr></thead>
-          <tbody><tr><th scope="row">Region</th><td>us-east-1</td></tr></tbody>
+          <thead>
+            <tr>
+              <th scope="col">Property</th>
+              <th scope="col">Value</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">Region</th>
+              <td>us-east-1</td>
+            </tr>
+          </tbody>
         </table>
       </PropertyTable>,
     );
