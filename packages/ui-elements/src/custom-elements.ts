c378382260d74d0510368d3be008aa1131c9b4b9
draglide <darkzzangelzz@gmail.com>
2026-09-24 10:33:12 +0700

    chore: regenerate Timeline Native projections

--- packages/ui-elements/src/custom-elements.ts
+++ packages/ui-elements/src/custom-elements.ts
@@ -49,6 +49,7 @@ export interface VyrnForgeHTMLElementTagNameMap {
   "vf-number-input": VyrnForgeElementInstance<"VyrnForgeNumberInputElement">;
   "vf-date-input": VyrnForgeElementInstance<"VyrnForgeDateInputElement">;
   "vf-description-list": VyrnForgeElementInstance<"VyrnForgeDescriptionListElement">;
+  "vf-timeline": VyrnForgeElementInstance<"VyrnForgeTimelineElement">;
   "vf-datetime-input": VyrnForgeElementInstance<"VyrnForgeDateTimeInputElement">;
   "vf-checkbox": VyrnForgeElementInstance<"VyrnForgeCheckboxElement">;
   "vf-radio": VyrnForgeElementInstance<"VyrnForgeRadioElement">;
