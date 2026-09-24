61145e47b98499c449abd4832eb4d72c3f55198f
draglide <darkzzangelzz@gmail.com>
2026-09-24 10:34:26 +0700

    style: match Prettier Timeline type layout

--- packages/ui-components/src/components/Timeline/Timeline.types.ts
+++ packages/ui-components/src/components/Timeline/Timeline.types.ts
@@ -2,10 +2,7 @@ import type { HTMLAttributes, LiHTMLAttributes, ReactElement } from "react";
 
 export type TimelineRef = HTMLOListElement;
 
-export type TimelineEntry = ReactElement<
-  LiHTMLAttributes<HTMLLIElement>,
-  "li"
->;
+export type TimelineEntry = ReactElement<LiHTMLAttributes<HTMLLIElement>, "li">;
 
 export type TimelineProps = Omit<
   HTMLAttributes<HTMLOListElement>,
